0ff494537d3a27aed8aed077b9d494db0a00781f
Kim McLane <kmmclan09@gmail.com>
2026-04-27 21:12:37 -0400

    another cors fix

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 //Middleware
 app.use(cors({
-    origin: ['https://action-figure-tracker.vercel.app/','http://localhost:5173'],
+    origin: ['https://action-figure-tracker.vercel.app','http://localhost:5173'],
     credentials: true
 }));
 app.use(express.json());
